6910c25b7a11c1714654a012b928ee55e2d4fa14
David <david.samaniego.8520@miremington.edu.co>
2024-03-09 00:15:56 -0500

    Pruebas de creacion e invocacion de componenetes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Cabeza from './Componentes/Componente_1/Cabeza';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Cabeza />
       </header>
     </div>
   );
