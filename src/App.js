f0a612825c75c9472a75c01caf2e05387e1ef931
David <david.samaniego.8520@miremington.edu.co>
2024-03-11 22:51:21 -0500

    Creacion del primer componente mas estilos e implementacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Cabeza from './Componentes/Componente_1/Cabeza';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="headerPrincipal">
         <Cabeza />
       </header>
+      <main>
+        
+      </main>
     </div>
   );
 }
--- src/Componentes/Componente_1/Cabeza.js
+++ src/Componentes/Componente_1/Cabeza.js
@@ -1,9 +1,21 @@
 import React from "react";
+import logo from "./logo.png";
+import carrito from "./carrito.svg"
+import "./Cabeza.css"
 
 export default function index(){
     return(
-        <div> 
-            Hola mundo
-        </div>  
+        <div class="container">
+            <h1 class="headerPrincipal-titulo" >
+                <img src={logo} alt="Logo" />
+                <a>Travel Goods</a>
+            </h1>
+            <nav class="headerPrincipal-nav">
+                <a class="headerPrincipal-nav-menu" href="#">INICIO</a>
+                <a class="headerPrincipal-nav-menu" href="#Productos">PRODUCTOS</a>
+                <a class="headerPrincipal-nav-menu" href="#Contacto">CONTACTO</a>
+            </nav>
+            <a href="#Carrito"><img src={carrito} /></a>
+        </div>
     );
 }
