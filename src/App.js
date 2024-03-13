fc44ac330900c58013de285ec85db56b2049ec09
David <david.samaniego.8520@miremington.edu.co>
2024-03-12 19:42:50 -0500

    Agregar componenete 4 resumen de la compra

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Cabeza from './Componentes/Componente_1/Cabeza';
 import Portada from './Componentes/Componente_2/Portada';
 import Produc from './Componentes/Componente_3/Produc';
+import Total from './Componentes/Componente_4/Total';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Portada />
         <Produc />
+        <Total />
       </main>
     </div>
   );
---
+++ src/Componentes/Componente_4/Total.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Total.css";
+
+export default function index(){
+    return(
+        <nav>
+                    <h2 class="contenidoPrincipal-subtitulo" id="Carrito">Carrito</h2>
+                    <div class="carritoContainer">
+                    </div>
+        </nav>
+    );
+}
